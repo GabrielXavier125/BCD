30a8d55ef3ae3e07acd964a4dacfa98b0da814e7
GabrielXavier125 <gx3848@gmail.com>
2025-08-07 16:34:28 -0300

    07.08.2025

---
+++ Banco de Dados Escola/Banco de Dados.sql
@@ -0,0 +1,15 @@
+create database exemplo;
+
+use exemplo;
+
+create table aluno (
+id_aluno int primary key
+);
+
+create table professor (
+id_aluno int primary key
+);
+
+create table curso (
+id_aluno int primary key
+);
